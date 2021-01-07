86a1fb5904d4be61747fbb026f66f9da18c1b3d6
James Cross <crossjames813@gmail.com>
2021-01-07 23:02:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   if (cardNumber!== undefined){
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart = []
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    
 }
 }
